040d373927e524a0513e4cc7a47cb0d9df1278a8
Sebastian Eder <sebastian.eder@qualicen.de>
2017-02-23 23:21:36 +0100

    Adding new build.

--- bundles/specmate-ui-core/webcontent/systemjs.config.js
+++ bundles/specmate-ui-core/webcontent/systemjs.config.js
@@ -6,7 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm:': 'lib/js/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -28,7 +28,7 @@
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
 
       // Bootstrap
-      '@ng-bootstrap/ng-bootstrap': 'node_modules/@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
+      '@ng-bootstrap/ng-bootstrap': 'npm:@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
