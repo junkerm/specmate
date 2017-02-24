98c7a4509463d4091dacb4c55046f40052c7ecc6
Maximilian Junker <junkerm@in.tum.de>
2017-02-24 17:27:23 +0100

    create bundle that configures the cdo persistency

---
+++ bundles/specmate-persistency-cdo/src/com/specmate/persistency/cdo/CDOPersistenceConfig.java
@@ -0,0 +1,8 @@
+package com.specmate.persistency.cdo;
+
+public class CDOPersistenceConfig {
+
+	public static final String PID = "com.specmate.persistency.cdo.internal.CDOPersistencyService";
+	public static final String KEY_REPOSITORY_NAME = "repositoryName";
+	public static final String KEY_RESOURCE_NAME = "resourceName";
+}
