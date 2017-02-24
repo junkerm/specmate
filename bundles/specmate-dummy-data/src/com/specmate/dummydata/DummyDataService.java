c5c52fe6bc268fe2ddfaeeb6b5037765a7a14e1f
Maximilian Junker <junkerm@in.tum.de>
2017-02-24 17:43:52 +0100

    dummy data only added once

--- bundles/specmate-dummy-data/src/com/specmate/dummydata/DummyDataService.java
+++ bundles/specmate-dummy-data/src/com/specmate/dummydata/DummyDataService.java
@@ -1,7 +1,7 @@
 package com.specmate.dummydata;
 
-import java.util.IllformedLocaleException;
-
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -11,21 +11,22 @@ import com.specmate.model.base.BaseFactory;
 import com.specmate.model.base.Folder;
 import com.specmate.model.requirements.Requirement;
 import com.specmate.model.requirements.RequirementsFactory;
+import com.specmate.model.support.util.SpecmateEcoreUtil;
 import com.specmate.persistency.IPersistencyService;
 import com.specmate.persistency.ITransaction;
 
 @Component(immediate = true)
 public class DummyDataService {
-	
+
 	private IPersistencyService persistencyService;
-	
+
 	@Reference
 	public void setPersistency(IPersistencyService persistencyService) {
 		this.persistencyService = persistencyService;
 	}
-	
+
 	private LogService logService;
-	
+
 	@Reference
 	public void setLogService(LogService logService) {
 		this.logService = logService;
@@ -34,23 +35,32 @@ public class DummyDataService {
 	@Activate
 	public void activate() {
 		ITransaction transaction = this.persistencyService.openTransaction();
-		
-		Folder folder = BaseFactory.eINSTANCE.createFolder();
-		folder.setId("Folder1");
-		folder.setName("My First Folder");
-		
-		Requirement requirement = RequirementsFactory.eINSTANCE.createRequirement();
-		requirement.setId("Requirement1");
-		requirement.setName("My Fresh Requirement");
-		
-		folder.getContents().add(requirement);
-		
-		transaction.getResource().getContents().add(folder);
-		
-		try {
-			transaction.commit();
-		} catch (Exception e) {
-			logService.log(LogService.LOG_ERROR, e.getMessage());
+		Resource resource = transaction.getResource();
+		EObject testData = SpecmateEcoreUtil.getEObjectWithName("test-data", resource.getContents());
+
+		if (testData != null) {
+			Folder testFolder = BaseFactory.eINSTANCE.createFolder();
+			testFolder.setId("test-data");
+			testFolder.setName("test-data");
+
+			Folder folder = BaseFactory.eINSTANCE.createFolder();
+			folder.setId("Folder1");
+			folder.setName("My First Folder");
+
+			Requirement requirement = RequirementsFactory.eINSTANCE.createRequirement();
+			requirement.setId("Requirement1");
+			requirement.setName("My Fresh Requirement");
+
+			folder.getContents().add(requirement);
+			testFolder.getContents().add(folder);
+
+			transaction.getResource().getContents().add(folder);
+
+			try {
+				transaction.commit();
+			} catch (Exception e) {
+				logService.log(LogService.LOG_ERROR, e.getMessage());
+			}
 		}
 	}
 }
