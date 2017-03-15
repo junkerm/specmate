f5e9a6c828c819386611773b7d91446908328bd2
Sebastian Eder <eders@in.tum.de>
2017-03-15 12:10:46 +0100

    Improved CEG Editor.

--- bundles/specmate-ui-core/webcontent/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var specmate_module_1 = require('./app/specmate.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(specmate_module_1.SpecmateModule);
-//# sourceMappingURL=main.js.map
--- bundles/specmate-ui-core/webcontent/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { SpecmateModule } from './app/specmate.module';
-
-platformBrowserDynamic().bootstrapModule(SpecmateModule);
