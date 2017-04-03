778cdded84d84b5386acdca2bb24dc5ac13fa2ee
Sebastian Eder <eders@in.tum.de>
2017-04-03 16:36:06 +0200

    adding docker stuff

--- docker/builddockerimage.sh
+++ docker/builddockerimage.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+cd ../bundles
 sh ../bundles/gradlew export
+cd ../docker
+
+cp ../bundles/specmate-std-env/generated/distributions/executable/specmate.jar .
 
 sudo docker build -t specmate/specmate:v0.1 .
 
+rm specmate.jar
