ae17c215ff979c0af43a5ebc735e4124cc9e1825
Sebastian Eder <eders@in.tum.de>
2017-04-05 10:36:51 +0200

    removing unnecessary sudo from build file

--- docker/builddockerimage.sh
+++ docker/builddockerimage.sh
@@ -10,6 +10,6 @@ cd ../docker
 
 cp ../bundles/specmate-std-env/generated/distributions/executable/specmate.jar .
 
-sudo docker build -t specmate/specmate:v0.1 .
+docker build -t specmate/specmate:v0.1 .
 
 rm specmate.jar
