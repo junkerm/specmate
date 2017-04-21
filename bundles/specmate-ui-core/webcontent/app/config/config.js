95884bbc91e51e2ddea40a59473ca4b6b709ee4e
Sebastian Eder <eders@in.tum.de>
2017-04-21 15:11:15 +0200

    Adding version string.

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -3,7 +3,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'b52944c'; 
+    Config.VERSION = 'f415ea3'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
