12fc3c78390191e84bb744efb80aa8d89200d536
Sebastian Eder <eders@in.tum.de>
2017-04-26 10:22:18 +0200

    Fixing labels for checkboxes checking other boxes.

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -3,7 +3,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'cc606e6'; 
+    Config.VERSION = '0d5da65'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
