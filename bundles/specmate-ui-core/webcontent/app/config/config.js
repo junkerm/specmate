6afeb38c94cdd8c4c766df33376c39b555edc9d6
Sebastian Eder <sebastian.eder@qualicen.de>
2017-05-05 17:18:35 +0200

    Update build number

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -3,7 +3,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '469023e'; 
+    Config.VERSION = '0726cf7'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
