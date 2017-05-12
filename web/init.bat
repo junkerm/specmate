bdd4f39fc177f3bff5e1e232032c23075752731e
Maximilian Junker <junkerm@in.tum.de>
2017-05-12 07:54:45 +0200

    ignore init.bat

--- web/init.bat
+++ web/init.bat
@@ -1,9 +1,3 @@
-SET HTTP_PROXY=surf.proxy.agis.allianz
-SET HTTPS_PROXY=surf.proxy.agis.allianz
-
-call npm config set proxy http://surf.proxy.agis.allianz:8080
-call npm config set https-proxy http://surf.proxy.agis.allianz:8080
-
 call npm install
 
 call npm run clean
