4326cb77509dacb5e977d0e909e17413e9edf540
Sebastian Eder <eders@in.tum.de>
2017-06-14 11:35:17 +0200

    Changing width of project explorer.

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -3,7 +3,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'ddc27e8'; 
+    Config.VERSION = '8ac72d2'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
