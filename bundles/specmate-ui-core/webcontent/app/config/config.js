12fc8ed1b15f8a75f568a525539460d024ea1c6a
Sebastian Eder <eders@in.tum.de>
2017-06-30 17:26:35 +0200

    Change order of node properties

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -3,7 +3,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'b9c22d7'; 
+    Config.VERSION = '2bd9020'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
