91503970f763abb67118983697ab4db3637a6f30
Sebastian Eder <eders@in.tum.de>
2017-07-05 13:53:03 +0200

    Changing button appearance

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -3,7 +3,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '9dd82e5'; 
+    Config.VERSION = 'aa0778e'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
