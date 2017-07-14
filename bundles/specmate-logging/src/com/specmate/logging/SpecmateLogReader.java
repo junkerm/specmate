50f3368716b86557b1e788f073ebd530309506cc
Sebastian Eder <eders@in.tum.de>
2017-07-14 12:27:03 +0200

    Changing log level to warning.

--- bundles/specmate-logging/src/com/specmate/logging/SpecmateLogReader.java
+++ bundles/specmate-logging/src/com/specmate/logging/SpecmateLogReader.java
@@ -65,7 +65,7 @@ public class SpecmateLogReader implements LogListener {
 
 	@Override
 	public void logged(LogEntry entry) {
-		if (entry.getLevel() > LogService.LOG_INFO) {
+		if (entry.getLevel() > LogService.LOG_WARNING) {
 			return;
 		}
 		String message = level2String.get(entry.getLevel()) + ":" + entry.getBundle().getSymbolicName() + ":"
