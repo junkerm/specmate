3a579a003a6da108e2e2964ca97494caf0abf23b
Sebastian Eder <eders@in.tum.de>
2017-08-09 17:26:45 +0200

    Adjusting color of hover/active links.

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '498cde7'; 
+    Config.VERSION = '9cf1673'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
