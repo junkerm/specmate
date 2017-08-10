12a3e227725517fa8ce5afa8924889f6a4d9a115
Sebastian Eder <sebastian.eder@qualicen.de>
2017-08-10 10:04:49 +0200

    Showing names of explorer elements on mouseover

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '9cf1673'; 
+    Config.VERSION = '3a579a0'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
