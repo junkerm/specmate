d04ce2c64d44317109f2ea8807f0b6398e4ddc1f
Sebastian Eder <sebastian.eder@qualicen.de>
2017-08-25 15:03:14 +0200

    Adding scrollbar to project explorer

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '15a0bc5'; 
+    Config.VERSION = 'e15acdc'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
