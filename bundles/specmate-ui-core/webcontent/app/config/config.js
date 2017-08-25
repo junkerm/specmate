1d34591b05ec6de8ed150d0f2b3854deed6b1c26
Sebastian Eder <sebastian.eder@qualicen.de>
2017-08-25 17:18:51 +0200

    Removing debug output

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'd2e6b67'; 
+    Config.VERSION = '13e88d0'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
