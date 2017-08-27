5dfba0fc45c08a39307cc88b32c32ac5f928631b
Sebastian Eder <sebastian.eder@qualicen.de>
2017-08-27 22:44:24 +0200

    Icons, minor

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '187f4db'; 
+    Config.VERSION = '971d1d5'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
