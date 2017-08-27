971d1d527ab5fd2dc81bc1795a449e40425499b4
Sebastian Eder <sebastian.eder@qualicen.de>
2017-08-27 22:35:40 +0200

    Changing generation button

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '5da71b4'; 
+    Config.VERSION = '187f4db'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
