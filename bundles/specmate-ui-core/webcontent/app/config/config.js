d69c0018dee6ab8a53fa1428b2dd981d6f8d9f81
Sebastian Eder <eders@in.tum.de>
2017-09-06 12:59:30 +0200

    Setting model to standalone for selected parameter

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '2182b7a'; 
+    Config.VERSION = 'e0a7bf9a'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
