53c2ee6c67aecb69ed1a0b4dd7d4d2c9892dd706
Sebastian Eder <eders@in.tum.de>
2017-09-13 09:28:23 +0200

    Adjusting referenced step numbers

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'bae13db1'; 
+    Config.VERSION = 'e54ab125'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
