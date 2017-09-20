1c898f28ca3ed3bcb7816f1b8d0a87c98024bbc2
Sebastian Eder <sebastian.eder@qualicen.de>
2017-09-20 14:49:52 +0200

    Adding Roboto Mono font

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'cc16548'; 
+    Config.VERSION = 'cbee3e8'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
