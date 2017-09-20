abe9a8825a5238e16a9cf328dc5879faba52e88c
Sebastian Eder <sebastian.eder@qualicen.de>
2017-09-20 07:55:56 +0200

    Removing debug dots

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '11b2c39'; 
+    Config.VERSION = 'fa7b045'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
