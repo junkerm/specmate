0e9cdf1c5d3950f29e06064b496626710c922cef
Sebastian Eder <sebastian.eder@qualicen.de>
2017-11-07 19:14:47 +0100

    Fix: Typo (implementing unit in requirements details perspective)

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '7c89670'; 
+    Config.VERSION = '921baac'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
