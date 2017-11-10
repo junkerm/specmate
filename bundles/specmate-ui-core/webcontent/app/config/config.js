ce3366632299d2c808ba5b333876b433b9268e14
Sebastian Eder <eders@in.tum.de>
2017-11-10 11:56:00 +0100

    Adding expand handles to processes

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'd9db9b45'; 
+    Config.VERSION = '1f3f9765'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
