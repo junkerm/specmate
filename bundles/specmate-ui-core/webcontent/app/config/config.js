cd47c42d4e7f87b932e457952a5bf8f1ccc09c8b
Sebastian Eder <eders@in.tum.de>
2017-11-10 12:22:05 +0100

    Making maximized class reusable

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'ce336663'; 
+    Config.VERSION = '2142afdb'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
