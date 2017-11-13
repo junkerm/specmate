cbec15f8126ff060584edbd3b56bd0e8271a5f47
Sebastian Eder <eders@in.tum.de>
2017-11-13 08:31:42 +0100

    Cleaning maximize css

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '2142afdb'; 
+    Config.VERSION = 'cd47c42d'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
