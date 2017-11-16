63d39923e60324acd08bec28846d8e0d8c357350
Sebastian Eder <eders@in.tum.de>
2017-11-16 13:31:32 +0100

    Cosmetic navbar change

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '9a098d86'; 
+    Config.VERSION = 'cc0ae9d9'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
