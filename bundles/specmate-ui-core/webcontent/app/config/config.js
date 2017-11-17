e3faeb27f540f6f2a423c6452450cf4c69fdef8a
Sebastian Eder <sebastian.eder@qualicen.de>
2017-11-17 16:39:58 +0100

    Adding alt texts to editor control buttons

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '96b7865'; 
+    Config.VERSION = '30dbb6c'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
