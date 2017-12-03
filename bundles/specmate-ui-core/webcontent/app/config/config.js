eb3ca9e8af7863ff30a746619238a25df4078053
Sebastian Eder <sebastian.eder@qualicen.de>
2017-12-03 12:21:53 +0100

    Moving tool pallette to into editor

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '81b8638'; 
+    Config.VERSION = 'c0bda76'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
