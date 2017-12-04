988bac85cffd5816653c4711c67221416a058bba
Sebastian Eder <sebastian.eder@qualicen.de>
2017-12-04 14:52:01 +0100

    Reqork links and actions

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '4218010'; 
+    Config.VERSION = 'bf735e3'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
