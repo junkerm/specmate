c33d3fc86a67343140d5d7e900b6986005858813
Sebastian Eder <sebastian.eder@qualicen.de>
2017-12-04 14:14:50 +0100

    Removing visible transitions

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = 'd0138c9'; 
+    Config.VERSION = '9313a0c'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
