b76492ef156fbeb2d7a3275082a4693265d29eb0
Sebastian Eder <sebastian.eder@qualicen.de>
2017-12-08 10:34:47 +0100

    Removing webpack files

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = /** @class */ (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '9718d20'; 
+    Config.VERSION = 'aef8b7d'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
--- bundles/specmate-ui-core/webcontent/polyfills.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("core-js/client/shim");
-require("zone.js/dist/zone");
-require("classlist.js");
-//# sourceMappingURL=polyfills.js.map
