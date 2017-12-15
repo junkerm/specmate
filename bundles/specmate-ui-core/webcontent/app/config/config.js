9200eeb86a9e5a70414898e32fe20dcfa8e35e16
Maximilian Junker <junkerm@in.tum.de>
2017-12-15 10:17:21 +0100

    - update rxjs version - adding some files to ignore list

--- bundles/specmate-ui-core/webcontent/app/config/config.js
+++ bundles/specmate-ui-core/webcontent/app/config/config.js
@@ -4,7 +4,7 @@ var Config = /** @class */ (function () {
     function Config() { 
     } 
     // VERSION is filled in by the build script. 
-    Config.VERSION = '07e0fc2'; 
+    Config.VERSION = '3fea76b0'; 
     Config.URL_BASE = 'services/rest/'; 
     Config.URL_CONTENTS = '/list'; 
     Config.URL_ELEMENT = '/details'; 
