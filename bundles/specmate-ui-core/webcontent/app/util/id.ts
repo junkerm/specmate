4081d311104eca7b7ec73abbcd20154cb21d4f06
Sebastian Eder <eders@in.tum.de>
2018-01-09 13:15:09 +0100

    Rebuild

---
+++ bundles/specmate-ui-core/webcontent/app/util/id.ts
@@ -0,0 +1,8 @@
+import { UUID } from 'angular2-uuid';
+
+export class Id {
+    /** Generates a new id (we hope we do not get duplicates. We do not check for that.) */
+    public static get uuid(): string {
+        return UUID.UUID();
+    }
+}
