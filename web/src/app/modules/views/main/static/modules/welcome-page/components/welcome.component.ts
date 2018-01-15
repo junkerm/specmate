0a097c34c2b05be7f29db3759ac9d69031e6785b
Sebastian Eder <sebastian.eder@qualicen.de>
2018-01-15 22:13:28 +0100

    First try of translated string in controller

--- web/src/app/modules/views/main/static/modules/welcome-page/components/welcome.component.ts
+++ web/src/app/modules/views/main/static/modules/welcome-page/components/welcome.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     moduleId: module.id.toString(),
     selector: 'welcome',
     templateUrl: 'welcome.component.html'
 })
-export class Welcome { }
+export class Welcome {
+
+    constructor(private translate: TranslateService) { }
+
+    public get str(): Observable<string> {
+        return this.translate.get('welcome.text');
+    }
+}
--- web/src/app/modules/views/main/static/modules/welcome-page/welcome-page.module.ts
+++ web/src/app/modules/views/main/static/modules/welcome-page/welcome-page.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Welcome } from './components/welcome.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
     // MODULE IMPORTS
-    TranslateModule
+    TranslateModule,
+    CommonModule
   ],
   declarations: [
     // COMPONENTS IN THIS MODULE
