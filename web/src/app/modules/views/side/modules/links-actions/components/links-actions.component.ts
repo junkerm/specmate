504ddaf52544cedd5376469bdb03671a07880131
janakaru <jana.rudolf@qualicen.de>
2018-01-29 15:20:05 +0100

    json code added in all html codes

--- web/src/app/modules/views/side/modules/links-actions/components/links-actions.component.ts
+++ web/src/app/modules/views/side/modules/links-actions/components/links-actions.component.ts
@@ -10,7 +10,7 @@ import { AdditionalInformationService } from '../services/additional-information
     templateUrl: 'links-actions.component.html',
     styleUrls: ['links-actions.component.css']
 })
-export class Actions {
+export class LinksActions {
 
     public _requirement: Requirement;
     public _model: IContainer;
