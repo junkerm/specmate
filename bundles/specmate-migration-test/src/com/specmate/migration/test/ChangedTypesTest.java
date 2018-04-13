f1ddfbb24cd768ee8ff6bda7072250d29a0e0383
Michael Unterkalmsteiner <mun@bth.se>
2018-04-13 14:57:11 +0200

    Lost import

--- bundles/specmate-migration-test/src/com/specmate/migration/test/ChangedTypesTest.java
+++ bundles/specmate-migration-test/src/com/specmate/migration/test/ChangedTypesTest.java
@@ -2,7 +2,7 @@ package com.specmate.migration.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotNull; 
 import static org.junit.Assert.assertTrue;
 
 import org.eclipse.emf.ecore.EObject;
