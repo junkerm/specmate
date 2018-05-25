ae0e1ca8d06098f9635ca1697f20f2e07183b5d7
Sebastian Eder <eders@in.tum.de>
2018-05-25 10:45:50 +0200

    Persisting language in cookie

--- web/src/app/modules/common/modules/i18n/components/language-chooser.component.ts
+++ web/src/app/modules/common/modules/i18n/components/language-chooser.component.ts
@@ -2,6 +2,7 @@ import { Config } from '../../../../../config/config';
 import { Component, Input, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
+import { CookieService } from 'ngx-cookie';
 
 @Component({
     selector: 'language-chooser',
@@ -10,13 +11,24 @@ import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
     providers: [NgbDropdownConfig]
 })
 export class LanguageChooser implements OnInit {
-    constructor(private translate: TranslateService, private config: NgbDropdownConfig) {
+
+    private static LANGUAGE_KEY = 'specmate-display-language';
+
+    constructor(private translate: TranslateService,
+        private cookie: CookieService,
+        private config: NgbDropdownConfig) {
         config.autoClose = true;
         config.placement = 'bottom-right';
     }
 
     public ngOnInit(): void {
         this.translate.addLangs(Config.LANGUAGES.map(languageObject => languageObject.code));
+        const cookieLang = this.retrieveFromCookie();
+        if (cookieLang !== undefined && cookieLang !== null && cookieLang.length > 0) {
+            this.translate.setDefaultLang(cookieLang);
+            this.language = cookieLang;
+            return;
+        }
         const browserLang = this.translate.getBrowserLang();
         if (Config.USE_BROWSER_LANGUAGE && this.translate.getLangs().indexOf(browserLang) > 0) {
             this.translate.setDefaultLang(browserLang);
@@ -29,6 +41,7 @@ export class LanguageChooser implements OnInit {
 
     public set language(language: string) {
         this.translate.use(language);
+        this.storeInCookie();
     }
 
     public get language(): string {
@@ -45,5 +58,13 @@ export class LanguageChooser implements OnInit {
 
     public get enabled(): boolean {
         return Config.LANGUAGE_CHOOSER_ENABLED;
+    }
+
+    private storeInCookie(): void {
+        this.cookie.put(LanguageChooser.LANGUAGE_KEY, this.language);
+    }
+
+    private retrieveFromCookie(): string {
+        return this.cookie.get(LanguageChooser.LANGUAGE_KEY);
     }
 }
