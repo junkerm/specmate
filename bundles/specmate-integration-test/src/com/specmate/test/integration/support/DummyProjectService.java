12bc382ff1798b5a91d27bfecfbf7fa48de249c8
Michael Unterkalmsteiner <mun@bth.se>
2018-06-01 10:36:26 +0200

    Use DummyProject and service in Authentication tests

--- bundles/specmate-integration-test/src/com/specmate/test/integration/support/DummyProjectService.java
+++ bundles/specmate-integration-test/src/com/specmate/test/integration/support/DummyProjectService.java
@@ -1,39 +1,28 @@
 package com.specmate.test.integration.support;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
 
 import com.specmate.connectors.api.IProjectService;
-import com.specmate.connectors.api.IRequirementsSource;
-import com.specmate.connectors.api.IExportService;
 import com.specmate.connectors.api.IProject;
 
 /**
  * Dummy implementation that does not require the config service. Pulling in the config service in the integration 
  * tests renders manual configuration impossible.
  */
-@Component(immediate = true)
+@Component(immediate = true, property= {"service.ranking:Integer=1"})
 public class DummyProjectService implements IProjectService {
+	private Map<String, IProject> projects = new HashMap<>();
 
 	@Override
 	public IProject getProject(String projectName) {
-		return new IProject() {
-
-			@Override
-			public String getName() {
-				return null;
-			}
-
-			@Override
-			public IRequirementsSource getConnector() {
-				return null;
-			}
-
-			@Override
-			public IExportService getExporter() {
-				return null;
-			}
-			
-		};
+		return projects.get(projectName);
+	}
+	
+	public void addProject(IProject project) {
+		projects.put(project.getName(), project);
 	}
 
 }
