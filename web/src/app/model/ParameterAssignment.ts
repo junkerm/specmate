f5379e11ab3bd4a08c0a9b885692b28f927a39d6
Dominik Spies <dominik.spies@qualicen.de>
2018-06-04 17:25:37 +0200

    Fixed Model Version for Typescript.

--- web/src/app/model/ParameterAssignment.ts
+++ web/src/app/model/ParameterAssignment.ts
@@ -4,7 +4,7 @@
 
 	export class ParameterAssignment  {
 
-		___nsuri: string = "http://specmate.com/20180510/model/testspecification";
+		___nsuri: string = "http://specmate.com/20180529/model/testspecification";
 		public url: string;
 		public className: string = "ParameterAssignment";
 		public static className: string = "ParameterAssignment";
