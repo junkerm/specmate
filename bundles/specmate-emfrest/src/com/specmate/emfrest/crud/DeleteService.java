9f00793b3e5db5b7f036b6e41c6c903d7f3d7d44
Maximilian Junker <maximilian.junker@qualicen.de>
2018-07-04 14:57:45 +0200

    add delete service

--- bundles/specmate-emfrest/src/com/specmate/emfrest/crud/DeleteService.java
+++ bundles/specmate-emfrest/src/com/specmate/emfrest/crud/DeleteService.java
@@ -1,7 +1,5 @@
 package com.specmate.emfrest.crud;
 
-import javax.ws.rs.core.Response;
-
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.osgi.service.component.annotations.Component;
@@ -12,7 +10,6 @@ import com.specmate.common.RestResult;
 import com.specmate.common.SpecmateException;
 import com.specmate.emfrest.api.IRestService;
 import com.specmate.emfrest.api.RestServiceBase;
-import com.specmate.model.support.util.SpecmateEcoreUtil;
 
 @Component(immediate = true, service = IRestService.class)
 public class DeleteService extends RestServiceBase {
@@ -30,12 +27,7 @@ public class DeleteService extends RestServiceBase {
 
 	@Override
 	public RestResult<?> delete(Object target, String token) throws SpecmateException {
-		if (target instanceof EObject && !(target instanceof Resource)) {
-			SpecmateEcoreUtil.detach((EObject) target);
-			return new RestResult<>(Response.Status.OK, target, authService.getUserName(token));
-		} else {
-			throw new SpecmateException("Attempt to delete non EObject");
-		}
+		return CrudUtil.delete(target, authService.getUserName(token));
 	}
 
 	@Reference
