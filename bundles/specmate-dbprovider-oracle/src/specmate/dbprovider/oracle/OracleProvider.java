5bd5bfef3cd32c8c701927bc09b321c1a73b3f29
Michael Unterkalmsteiner <mun@bth.se>
2018-07-09 22:37:23 +0200

    Revert "Remove oracle bundle since it does not compile on github"
    This reverts commit 7611d0e29c389a7e8e3e45fc15a248a4d00469d8.

---
+++ bundles/specmate-dbprovider-oracle/src/specmate/dbprovider/oracle/OracleProvider.java
@@ -0,0 +1,180 @@
+package specmate.dbprovider.oracle;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Dictionary;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.emf.cdo.server.IStore;
+import org.eclipse.emf.cdo.server.db.CDODBUtil;
+import org.eclipse.emf.cdo.server.db.mapping.IMappingStrategy;
+import org.eclipse.net4j.db.DBUtil;
+import org.eclipse.net4j.db.IDBAdapter;
+import org.eclipse.net4j.db.IDBConnectionProvider;
+import org.eclipse.net4j.db.oracle.OracleAdapter;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Reference;
+
+import com.specmate.common.SpecmateException;
+import com.specmate.dbprovider.api.IDBProvider;
+
+import oracle.jdbc.pool.OracleDataSource;
+import specmate.dbprovider.oracle.config.OracleProviderConfig;
+
+@Component(service = IDBProvider.class, configurationPid = OracleProviderConfig.PID, configurationPolicy = ConfigurationPolicy.REQUIRE, property = {
+		"service.ranking:Integer=1" })
+public class OracleProvider implements IDBProvider {
+
+	private ConfigurationAdmin configurationAdmin;
+	private Connection connection;
+	private String jdbcConnection;
+	private String repository;
+	private String resource;
+	private String userResource;
+	private String username;
+	private String password;
+
+	@Activate
+	public void activate() throws SpecmateException {
+		readConfig();
+	}
+
+	@Override
+	public Connection getConnection() throws SpecmateException {
+		if (this.connection == null) {
+			initiateDBConnection();
+		}
+
+		return this.connection;
+	}
+
+	@Override
+	public void closeConnection() throws SpecmateException {
+		if (this.connection != null) {
+			try {
+				connection.close();
+				connection = null;
+			} catch (SQLException e) {
+				throw new SpecmateException("Could not close connection.", e);
+			}
+		}
+	}
+
+	@Override
+	public void readConfig() throws SpecmateException {
+		try {
+			Dictionary<String, Object> configProperties = configurationAdmin.getConfiguration(OracleProviderConfig.PID)
+					.getProperties();
+			this.jdbcConnection = (String) configProperties.get(OracleProviderConfig.KEY_JDBC_CONNECTION);
+			this.repository = (String) configProperties.get(OracleProviderConfig.KEY_REPOSITORY_NAME);
+			this.resource = (String) configProperties.get(OracleProviderConfig.KEY_RESOURCE_NAME);
+			this.userResource = (String) configProperties.get(OracleProviderConfig.KEY_USER_RESOURCE_NAME);
+			this.username = (String) configProperties.get(OracleProviderConfig.KEY_USERNAME);
+			this.password = (String) configProperties.get(OracleProviderConfig.KEY_PASSWORD);
+
+			String failmsg = " not defined in configuration.";
+
+			if (!StringUtils.isNotEmpty(this.jdbcConnection)) {
+				throw new SpecmateException("JDBC connection" + failmsg);
+			}
+
+			if (!StringUtils.isNotEmpty(this.repository)) {
+				throw new SpecmateException("Database repository" + failmsg);
+			}
+
+			if (!StringUtils.isNotEmpty(this.resource)) {
+				throw new SpecmateException("Database resource" + failmsg);
+			}
+
+			if (!StringUtils.isNotEmpty(this.userResource)) {
+				throw new SpecmateException("Database user resource" + failmsg);
+			}
+
+			if (!StringUtils.isNotEmpty(this.username)) {
+				throw new SpecmateException("Database user name" + failmsg);
+			}
+
+			if (!StringUtils.isNotEmpty(this.password)) {
+				throw new SpecmateException("Database password" + failmsg);
+			}
+		} catch (IOException e) {
+			throw new SpecmateException("Could not obtain database configuration.", e);
+		}
+
+	}
+
+	@Override
+	public String getResource() {
+		return this.resource;
+	}
+
+	@Override
+	public String getUserResource() {
+		return this.userResource;
+	}
+
+	@Override
+	public String getRepository() {
+		return this.repository;
+	}
+
+	@Override
+	public IStore getStore() throws SpecmateException {
+		IStore store;
+		try {
+			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+			OracleDataSource odataSource = new OracleDataSource();
+			odataSource.setURL(this.jdbcConnection);
+			odataSource.setUser(this.username);
+			odataSource.setPassword(this.password);
+			IMappingStrategy omappingStrategy = CDODBUtil.createHorizontalMappingStrategy(true);
+			IDBAdapter odbAdapter = new OracleAdapter();
+			IDBConnectionProvider odbConnectionProvider = DBUtil.createConnectionProvider(odataSource);
+			store = CDODBUtil.createStore(omappingStrategy, odbAdapter, odbConnectionProvider);
+		} catch (SQLException e) {
+			throw new SpecmateException("Could not create Oracle data store.", e);
+		}
+
+		return store;
+	}
+
+	@Override
+	public boolean isVirginDB() throws SpecmateException {
+		Connection connection = getConnection();
+		try {
+			PreparedStatement stmt = connection.prepareStatement("select * from CDO_PACKAGE_INFOS");
+			stmt.execute();
+		} catch (SQLException e) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private void initiateDBConnection() throws SpecmateException {
+		try {
+			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+		} catch (SQLException e) {
+			throw new SpecmateException("Migration: Could not register Oracle driver.", e);
+		}
+
+		try {
+			this.connection = DriverManager.getConnection(this.jdbcConnection, this.username, this.password);
+		} catch (SQLException e) {
+			throw new SpecmateException("Migration: Could not connect to the Oracle database using the connection: "
+					+ this.jdbcConnection + ".", e);
+		}
+	}
+
+	@Reference
+	public void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
+		this.configurationAdmin = configurationAdmin;
+	}
+
+}
