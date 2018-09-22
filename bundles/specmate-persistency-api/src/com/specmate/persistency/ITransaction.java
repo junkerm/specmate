59f5f188bf2eb13cd5067518a77911ba7b0dbb56
Michael Unterkalmsteiner <mun@bth.se>
2018-09-22 17:53:39 +0200

    Remove commit() from the API to prevent use

--- bundles/specmate-persistency-api/src/com/specmate/persistency/ITransaction.java
+++ bundles/specmate-persistency-api/src/com/specmate/persistency/ITransaction.java
@@ -10,17 +10,6 @@ import com.specmate.common.SpecmateValidationException;
  *
  */
 public interface ITransaction extends IView {
-
-	/**
-	 * Commits changes done in this transaction to the repository
-	 *
-	 * @throws SpecmateException
-	 *             If the commit could not be performed successfully
-	 */
-	public void commit() throws SpecmateException;
-
-	public <T> void commit(T object) throws SpecmateException;
-
 	/** Rolls back changes made in this transaction since the last commit */
 	public void rollback();
 
