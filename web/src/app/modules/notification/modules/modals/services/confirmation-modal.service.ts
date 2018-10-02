41ee38f785a18fce1ae8ffe4522af1ce8dd88b8d
manpreethb <37306787+manpreethb@users.noreply.github.com>
2018-10-02 18:36:14 +0200

    Updated "confirmDelete" so that there is no duplication
    The openOkCancel function on line 30 is been removed so that it does not duplicate the confirmDelete function on line 18

--- web/src/app/modules/notification/modules/modals/services/confirmation-modal.service.ts
+++ web/src/app/modules/notification/modules/modals/services/confirmation-modal.service.ts
@@ -25,12 +25,7 @@ export class ConfirmationModal {
         const modalRef = this.modalService.open(TypedModalContent);
         modalRef.componentInstance.options = Dialogtype.okDialog(title, message);
         return modalRef.result;
-    }
-
-    public openOkCancel(title: string, message: string): Promise<any> {
-        const modalRef = this.modalService.open(TypedModalContent);
-        modalRef.componentInstance.options = Dialogtype.okCancelDialog(title, message);
-        return modalRef.result;
+    
     }
 
     public confirmSave(message?: string): Promise<void> {
