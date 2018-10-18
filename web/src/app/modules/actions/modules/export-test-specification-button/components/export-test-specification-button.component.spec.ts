c5af838ae62394eaddaa6d46f7bc096f09a81046
Giannis Mavroudopoulos <mavroudo@csd.auth.gr>
2018-10-18 08:55:19 +0200

    this should work but we need to test it to a browser, that means we need to use the jasmine-karma testing , i think

--- web/src/app/modules/actions/modules/export-test-specification-button/components/export-test-specification-button.component.spec.ts
+++ web/src/app/modules/actions/modules/export-test-specification-button/components/export-test-specification-button.component.spec.ts
@@ -1,12 +1,24 @@
-import { ExportTestSpecificationButton } from './export-test-specification-button.component'
+import { ExportTestSpecificationButton } from './export-test-specification-button.component';
 
 import { expect } from 'chai';
 import 'mocha';
+import { JSDOM } from 'jsdom';
 
 describe('Export test specification button', () => {
     it('should return a csv', () => {
-        var file = new Blob(["line1", "line2"], {type: 'text/csv'});
-        ExportTestSpecificationButton.exportAsCSV(file);
-        expect(file).to.contain("line1", "line2");
+        const jsdom      = new JSDOM('<!DOCTYPE HTML><html><body></body></html>');
+        let blob = new Blob(['helloWorld\n'], { type: 'text/csv' });
+        let url = window.URL.createObjectURL(blob);
+        if (navigator.msSaveOrOpenBlob) {
+            navigator.msSaveBlob(blob, 'Book.csv');
+        } else {
+            let a = document.createElement('a');
+            a.href = url;
+            a.download = 'Book.csv';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+        window.URL.revokeObjectURL(url);
     });
 });
