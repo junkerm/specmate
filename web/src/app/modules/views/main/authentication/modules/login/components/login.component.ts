b1ddfcb0313b642fc7a07e8bc400fc6090e4f2c9
anushamogili <37270878+anushamogili@users.noreply.github.com>
2018-10-26 23:42:17 +0200

    Group 4 feature 7 variant 2 (mandatory) -: the user can select and configure the background image. In addition, the user can upload several background images and on each reload of the login page, Specmate shows a random image
    1.We have used input textbox with type file to upload images. An array is used to store the URL of uploaded images and background image of specmate is implemented.
    
    2.  On each reload of the login page, Specmate shows a random image from the user uploaded images for this we have saved the array in session. When the page is reloaded the array images which are present in session is retrieved and displays randomly.
    
    3.  Uploaded images are displayed on the right side of the page. We can also choose image from the uploaded images to set as background image.
    4.Images should be uploaded again if tab is closed

--- web/src/app/modules/views/main/authentication/modules/login/components/login.component.ts
+++ web/src/app/modules/views/main/authentication/modules/login/components/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../auth/services/authentication.service';
 import { User } from '../../../../../../../model/User';
 import { NavigatorService } from '../../../../../../navigation/modules/navigator/services/navigator.service';
+import {Observable} from 'rxjs';
+import { stringify } from 'querystring';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
     moduleId: module.id.toString(),
@@ -9,12 +12,14 @@ import { NavigatorService } from '../../../../../../navigation/modules/navigator
     templateUrl: 'login.component.html',
     styleUrls: ['login.component.css']
 })
-export class Login implements OnInit {
+
+export class Login implements OnInit, AfterViewInit {
     public username = '';
     public password = '';
     public _project = '';
     public projectnames: string[];
-
+    public body: any;
+    public bgImage = 'https://www.backgroundcheckadvantage.com/userfiles/com.background/image/bca-slide___Source.jpg';
     public isAuthenticating = false;
 
     constructor(private auth: AuthenticationService, private navigator: NavigatorService) {
@@ -32,6 +37,69 @@ export class Login implements OnInit {
 
     ngOnInit() {
         this.tryNavigateAway();
+    }
+     name = 'Angular 4';
+  urls: string[]  = new Array(10);
+selectedFile: File;
+    onSelectFile(event: { target: { files: Blob[]; }; }) {
+     this.selectedFile = <File>event.target.files[0];
+        if (event.target.files && event.target.files[0]) {
+          let reader = new FileReader();
+    reader.readAsDataURL(this.selectedFile); // read file as data url
+    reader.onload = (event) => { // called once readAsDataURL is completed
+        this.urls.push(<string>reader.result);
+        sessionStorage.clear();
+        sessionStorage.setItem('urls' , JSON.stringify(this.urls));
+        console.log('added new img');
+        console.log(this.urls);
+        this.body = document.getElementsByTagName('body')[0];
+       if ( this.body ) {
+            this.body.style.backgroundImage = "url('" + reader.result + "')";
+            // this.body.style.backgroundRepeat = 'no-repeat' ;
+        }
+          };
+        }
+      }
+      setBGImage(url: string) {
+        this.body = document.getElementsByTagName('body')[0];
+        if ( this.body ) {
+             this.body.style.backgroundImage = "url('" + url + "')";
+             // this.body.style.backgroundRepeat = 'no-repeat' ;
+         }
+      }
+    setBackGround() {
+        console.log('background setting....');
+console.log(this.urls.length);
+        if ( this.urls.length != 0 ) {
+             let index: number = this.getRndInteger(0, this.urls.length - 1);
+             this.body = document.getElementsByTagName('body')[0];
+            console.log(index);
+            console.log(this.urls[index]);
+         if ( this.body && (this.urls[index] != undefined || this.urls[index] != null)) {
+            this.body.style.backgroundImage = "url('" + this.urls[index] + "')";
+            // this.body.style.backgroundRepeat = 'no-repeat' ;
+         } else if (this.body) {
+            this.body.style.backgroundImage = "url('" + this.bgImage + "')";
+            // this.body.style.backgroundRepeat = 'no-repeat' ;
+         }
+        }
+    }
+     getRndInteger(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+    ngAfterViewInit(): void {
+        let urlStr: string = sessionStorage.getItem('urls');
+        if (urlStr != null || urlStr != undefined) {
+            console.log(urlStr);
+        this.urls = JSON.parse(JSON.parse(JSON.stringify(urlStr)));
+        this.setBackGround();
+        } else {
+        this.body = document.getElementsByTagName('body')[0];
+       if ( this.body ) {
+            this.body.style.backgroundImage = "url('" + this.bgImage + "')";
+        }
+    }
+    
     }
 
     private tryNavigateAway(): void {
@@ -52,6 +120,7 @@ export class Login implements OnInit {
         await this.auth.authenticate(user);
         this.tryNavigateAway();
         this.isAuthenticating = false;
+        this.body.style.backgroundImage = null;
         return Promise.resolve(this.auth.isAuthenticated);
     }
 
