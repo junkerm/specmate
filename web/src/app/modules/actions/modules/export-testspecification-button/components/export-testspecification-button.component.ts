66231569f1c8446dd5243627ed46c17c42f8477c
Weeds <waleed.totti@gmail.com>
2018-11-08 15:28:40 +0100

    Added file-saver package
    -added file-saver package to the dependencies 
    - used saveAs from file-saver inside create file method

--- web/src/app/modules/actions/modules/export-testspecification-button/components/export-testspecification-button.component.ts
+++ web/src/app/modules/actions/modules/export-testspecification-button/components/export-testspecification-button.component.ts
@@ -12,6 +12,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { AuthenticationService } from '../../../../views/main/authentication/modules/auth/services/authentication.service';
 import { AccessRights } from '../../../../../model/AccessRights';
 import { UserToken } from '../../../../views/main/authentication/base/user-token';
+import { saveAs } from 'file-saver';
 
 @Component({
     moduleId: module.id.toString(),
@@ -143,18 +144,7 @@ export class ExportTestspecificationButton {
 
     private createDownloadFile(): void {
       const blob = new Blob(['\ufeff', this.finalCsvString], { type: 'text/csv;charset=utf-8;' });
-      const url = window.URL.createObjectURL(blob);
-      if (navigator.msSaveOrOpenBlob) {
-          navigator.msSaveBlob(blob, this.testSpecification.name + '.csv');
-      } else {
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = this.testSpecification.name + '.csv';
-          document.body.appendChild(a);
-          a.click();
-          document.body.removeChild(a);
-      }
-      window.URL.revokeObjectURL(url);
+      saveAs(blob, this.testSpecification.name + '.csv');
     }
 
     public get buttonTitle(): string {
