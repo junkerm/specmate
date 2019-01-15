122fe78b4a46d167c2bf0fde7ae419daa2d895c0
Tobias Springer <tobias.springer@tum.de>
2019-01-15 11:39:51 +0100

    Add: Tunnel identifier

--- ui-tests/src/main/java/de/qualicen/maven/ui_tests/Testing.java
+++ ui-tests/src/main/java/de/qualicen/maven/ui_tests/Testing.java
@@ -22,12 +22,14 @@ public class Testing {
 	  public static final String USERNAME = System.getenv("SAUCE_USERNAME");
 	  public static final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
 	  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+	  public static final String TUNNELIDENTFIER = System.getenv("TRAVIS_JOB_NUMBER");
 	
 	public static void main(String[] args) throws InterruptedException, MalformedURLException {
 		
 		DesiredCapabilities caps = DesiredCapabilities.chrome();
 	    caps.setCapability("platform", "Windows 10");
 	    caps.setCapability("version", "latest");
+	    caps.setCapability("tunnel-identifier", TUNNELIDENTFIER);
 	 
 	    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
 
