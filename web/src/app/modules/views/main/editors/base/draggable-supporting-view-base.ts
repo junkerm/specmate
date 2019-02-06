796d192590c5cc63c1ef444d376a6ec6afe89b59
Sebastian Eder <23170307+sebeder@users.noreply.github.com>
2019-02-06 16:32:41 +0100

    Remove unnecessary code

--- web/src/app/modules/views/main/editors/base/draggable-supporting-view-base.ts
+++ web/src/app/modules/views/main/editors/base/draggable-supporting-view-base.ts
@@ -44,7 +44,6 @@ export abstract class DraggableSupportingViewBase extends SpecmateViewBase {
         private dragulaService: DragulaService,
         translate: TranslateService) {
         super(dataService, navigator, route, modal, translate);
-        // this.dragulaService.drop(this.dndBagName).subscribe((args) => this.onDrop(args));
         this.dragulaService.dropModel(this.dndBagName)
             .subscribe(({sourceIndex, targetIndex}) => this.onDropModel(sourceIndex, targetIndex));
     }
@@ -55,11 +54,6 @@ export abstract class DraggableSupportingViewBase extends SpecmateViewBase {
         const sourceContentIndex = this.contents.indexOf(source);
         const targetContentIndex = this.contents.indexOf(target);
         this.contents.splice(targetContentIndex, 0, this.contents.splice(sourceContentIndex, 1)[0]);
-        this.sanitizeContentPositions(true);
-    }
-
-    private onDrop(args: any): void {
-        console.log(args);
         this.sanitizeContentPositions(true);
     }
 
