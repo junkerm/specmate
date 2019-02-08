c27eceaf786285a720c1d8da83da3b110bd64d96
sebeder <23170307+sebeder@users.noreply.github.com>
2019-02-08 11:15:18 +0100

    Major cleanup of data interface (and fixes) (#384)
    * Major cleanup of data interface (and fixes)
    
    * Restoring read element method

--- web/src/app/modules/data/modules/data-service/services/service-interface.ts
+++ web/src/app/modules/data/modules/data-service/services/service-interface.ts
@@ -1,47 +1,33 @@
-import { HttpClient, HttpResponse, HttpParams, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Url } from '../../../../../util/url';
 import { IContainer } from '../../../../../model/IContainer';
 import { Objects } from '../../../../../util/objects';
 import { CEGConnection } from '../../../../../model/CEGConnection';
 import { Type } from '../../../../../util/type';
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
-import { retryWhen, mergeMap, finalize, catchError } from 'rxjs/operators';
 import { _throw } from 'rxjs/observable/throw';
 import { UserToken } from '../../../../views/main/authentication/base/user-token';
 import { UserSession } from '../../../../../model/UserSession';
 import { User } from '../../../../../model/User';
 import { BatchOperation } from '../../../../../model/BatchOperation';
-import { Config } from '../../../../../config/config';
-import { timer } from 'rxjs/observable/timer';
-import { PromiseObservable } from 'rxjs/observable/PromiseObservable';
 
 export class ServiceInterface {
 
     constructor(private http: HttpClient) { }
 
-    public checkConnection(token?: UserToken): Promise<void> {
+    public async checkConnection(token?: UserToken): Promise<void> {
         if (token === undefined || token === UserToken.INVALID) {
             const error = new HttpErrorResponse({ status: 401 });
             return Promise.reject(error);
         }
 
-        const retryOptions = new RetryStrategyOptions();
-
-        retryOptions.maxRetryAttempts = 2;
-        retryOptions.includedStatusCodes = [404];
-        retryOptions.delay = 100;
-
         let params = new HttpParams();
         params = params.append('heartbeat', 'true');
-        return this.toRetryPromise(this.http
-            .get(Url.urlCheckConnectivity(token.project), { headers: this.getAuthHeader(token), params: params }), retryOptions)
-            .then(result => {
-                if (result instanceof HttpErrorResponse) {
-                    return Promise.reject(result);
-                }
-                return Promise.resolve();
-            });
+        const result = await this.http.get(Url.urlCheckConnectivity(token.project), { headers: this.getAuthHeader(token), params: params })
+            .toPromise();
+        if (result instanceof HttpErrorResponse) {
+            return Promise.reject(result);
+        }
     }
 
     public async authenticate(user: User): Promise<UserToken> {
@@ -49,69 +35,58 @@ export class ServiceInterface {
         return new UserToken(session, user.projectName, session.libraryFolders);
     }
 
-    public deauthenticate(token: UserToken): Promise<void> {
-        return this.toRetryPromise(this.http
-            .get(Url.urlDeauthenticate(), { headers: this.getAuthHeader(token), responseType: 'text' }))
-            .then(() => Promise.resolve());
+    public async deauthenticate(token: UserToken): Promise<void> {
+        await  this.http.get(Url.urlDeauthenticate(), { headers: this.getAuthHeader(token), responseType: 'text' }).toPromise();
     }
 
     public async projectnames(): Promise<string[]> {
-        return this.toRetryPromise(this.http.get<string[]>(Url.urlProjectNames()));
+        return this.http.get<string[]>(Url.urlProjectNames()).toPromise();
     }
 
     public async performBatchOperation(batchOperation: BatchOperation, token: UserToken): Promise<void> {
-        return this.toRetryPromise(this.http
-            .post<void>(Url.batchOperationUrl(token), batchOperation, { headers: this.getAuthHeader(token) }));
+        return this.http
+            .post<void>(Url.batchOperationUrl(token), batchOperation, { headers: this.getAuthHeader(token) })
+            .toPromise();
     }
 
-    public createElement(element: IContainer, token: UserToken): Promise<void> {
+    public async createElement(element: IContainer, token: UserToken): Promise<void> {
         let payload: any = this.prepareElementPayload(element);
-        return this.toRetryPromise(this.http
-            .post(Url.urlCreate(element.url), payload, { headers: this.getAuthHeader(token) }), new RetryStrategyOptions(), element.url)
-            .then(() => { });
+        await this.http.post(Url.urlCreate(element.url), payload, { headers: this.getAuthHeader(token) }).toPromise();
     }
 
-    public readElement(url: string, token: UserToken): Promise<IContainer> {
-        return this.toRetryPromise(this.http
-            .get<IContainer>(Url.urlElement(url), { headers: this.getAuthHeader(token) }), new RetryStrategyOptions(), url)
-            .then((element: IContainer) => element);
+    public async readElement(url: string, token: UserToken): Promise<IContainer> {
+        const element = await this.http.get<IContainer>(Url.urlElement(url), { headers: this.getAuthHeader(token) }).toPromise();
+        return element;
     }
 
-    public readContents(url: string, token: UserToken): Promise<IContainer[]> {
-        return this.toRetryPromise(this.http
-            .get<IContainer[]>(Url.urlContents(url), { headers: this.getAuthHeader(token) }), new RetryStrategyOptions(), url)
-            .then((contents: IContainer[]) => contents);
+    public async readContents(url: string, token: UserToken): Promise<IContainer[]> {
+        const contents = this.http.get<IContainer[]>(Url.urlContents(url), { headers: this.getAuthHeader(token) }).toPromise();
+        return contents;
     }
 
-    public updateElement(element: IContainer, token: UserToken): Promise<void> {
+    public async updateElement(element: IContainer, token: UserToken): Promise<void> {
         let payload: any = this.prepareElementPayload(element);
-        return this.toRetryPromise(this.http
-            .put(Url.urlUpdate(element.url), payload, { headers: this.getAuthHeader(token) }), new RetryStrategyOptions(), element.url);
+        await this.http.put(Url.urlUpdate(element.url), payload, { headers: this.getAuthHeader(token) }).toPromise();
     }
 
-    public deleteElement(url: string, token: UserToken): Promise<void> {
-        return this.toRetryPromise(this.http
-            .delete(Url.urlDelete(url), { headers: this.getAuthHeader(token) }), new RetryStrategyOptions(), url);
+    public async deleteElement(url: string, token: UserToken): Promise<void> {
+        await this.http.delete(Url.urlDelete(url), { headers: this.getAuthHeader(token) }).toPromise();
     }
 
-    public performOperation(url: string, serviceSuffix: string, payload: any, token: UserToken): Promise<void> {
-        return this.toRetryPromise(this.http
-            .post(Url.urlCustomService(url, serviceSuffix), payload,
-                { headers: this.getAuthHeader(token) }), new RetryStrategyOptions(),
-            url);
+    public async performOperation(url: string, serviceSuffix: string, payload: any, token: UserToken): Promise<void> {
+        await this.http.post(Url.urlCustomService(url, serviceSuffix), payload, { headers: this.getAuthHeader(token) }).toPromise();
     }
 
-    public performQuery(url: string, serviceSuffix: string, parameters: { [key: string]: string }, token: UserToken): Promise<any> {
+    public async performQuery(url: string, serviceSuffix: string, parameters: { [key: string]: string }, token: UserToken): Promise<any> {
         let urlParams = new HttpParams();
         for (let key in parameters) {
             if (parameters[key]) {
                 urlParams = urlParams.append(key, parameters[key]);
             }
         }
-        return this.toRetryPromise(this.http
-            .get(Url.urlCustomService(url, serviceSuffix),
-                { params: urlParams, headers: this.getAuthHeader(token) }), new RetryStrategyOptions(), url)
-            .then((data: any) => data);
+        const result = await this.http
+            .get(Url.urlCustomService(url, serviceSuffix), { params: urlParams, headers: this.getAuthHeader(token) }).toPromise();
+        return result;
     }
 
     /** Perform a model search.
@@ -121,7 +96,7 @@ export class ServiceInterface {
      *                  If a search field begins with '-', this means results that match the query should be excluded.
      *                  Example: {'-name':'car'} --> Exclude results with 'car' in the name
      */
-    public search(query: string, token: UserToken, filter?: { [key: string]: string }): Promise<IContainer[]> {
+    public async search(query: string, token: UserToken, filter?: { [key: string]: string }): Promise<IContainer[]> {
         let urlParams: HttpParams = new HttpParams();
         let queryString = query ? '+(' + query + ')' : '';
         if (filter) {
@@ -136,37 +111,18 @@ export class ServiceInterface {
             }
         }
         urlParams = urlParams.append('query', queryString);
-        return this.http
-            .get<IContainer[]>(Url.urlCustomService(token.project, 'search'), { params: urlParams, headers: this.getAuthHeader(token) })
-            .toPromise()
-            .catch(this.handleError)
-            .then((response: IContainer[]) => response);
-    }
 
-    private getParamsForQueryString(query: string): HttpParams {
-        let phraseRegex: RegExp = new RegExp('\"[^\"]\"');
-        let foundPhrase = '';
-        let foundPhrases: string[] = [];
-        do {
-            let foundPhrase = phraseRegex.exec(query);
-            if (foundPhrase) {
-                foundPhrases.push(foundPhrase[0]);
-            }
-        } while (foundPhrase);
-        query = query.replace(phraseRegex, '');
-        let searchTerms: string[] = query.split(new RegExp('\s+'));
-        searchTerms = searchTerms.concat(foundPhrases);
-
-        let urlParams: HttpParams = new HttpParams();
-        for (let i = 0; i < searchTerms.length; i++) {
-            urlParams = urlParams.append('name', searchTerms[i]);
-            urlParams = urlParams.append('description', searchTerms[i]);
-            urlParams = urlParams.append('extid', searchTerms[i]);
+        try {
+            const response = await this.http
+                .get<IContainer[]>(Url.urlCustomService(token.project, 'search'), { params: urlParams, headers: this.getAuthHeader(token) })
+                .toPromise();
+            return response;
+        } catch (e) {
+            this.handleError(e);
         }
-        return urlParams;
     }
 
-    private handleError(error: any, url?: string): Promise<any> {
+    private async handleError(error: any, url?: string): Promise<any> {
         console.error('Error in Service Interface! (details below) [' + url + ']');
         return Promise.reject(error);
     }
@@ -192,31 +148,4 @@ export class ServiceInterface {
         }
         return headers;
     }
-
-    private toRetryPromise(req: Observable<any>, options: RetryStrategyOptions = new RetryStrategyOptions(), url?: string): Promise<any> {
-        return req.pipe(
-            retryWhen(genericRetryStrategy(options)),
-            catchError(error => Observable.of(error)))
-            .toPromise().catch(e => this.handleError(e, url));
-    }
 }
-
-class RetryStrategyOptions {
-    public maxRetryAttempts: number = Config.NUM_HTTP_RETRIES;
-    public delay: number = Config.HTTP_RETRY_DELAY;
-    public includedStatusCodes: number[] = Config.HTTP_RETRY_ERRORS;
-}
-
-export const genericRetryStrategy = (options: RetryStrategyOptions) => (attempts: Observable<any>) => {
-    return attempts.pipe(
-        mergeMap((error, i) => {
-            const retryAttempt = i + 1;
-            if (retryAttempt > options.maxRetryAttempts || options.includedStatusCodes.indexOf(error.status) < 0) {
-                return _throw(error);
-            }
-            console.log(`Attempt ${retryAttempt}: retrying in ${options.delay}ms`);
-            return timer(options.delay);
-        }),
-        finalize(() => { })
-    );
-};
