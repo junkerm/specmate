1e472de4cf8e5778ff18222a28cb7ace7e856700
tobi321 <43335561+tobi321@users.noreply.github.com>
2019-02-20 16:57:11 +0100

    Update CEGEditorElements.java

--- ui-tests/src/main/java/SpecmatePageClasses/CEGEditorElements.java
+++ ui-tests/src/main/java/SpecmatePageClasses/CEGEditorElements.java
@@ -77,6 +77,12 @@ public class CEGEditorElements {
 		builder.moveToElement(editorField, x, y).click().build().perform();
 		
 		
+		WebDriverWait wait=new WebDriverWait(driver, 120);
+		
+		
+		WebElement node = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@generic-graphical-node][" + numberOfNodes + "]//*[@ceg-graphical-node]")));
+		
+		
 		WebElement node = driver.findElement(By.xpath("//*[@generic-graphical-node][" + numberOfNodes + "]//*[@ceg-graphical-node]"));
 		WebElement variableTextfield = driver.findElement(propertiesVariable);
 		WebElement conditionTextfield = driver.findElement(propertiesCondition);
