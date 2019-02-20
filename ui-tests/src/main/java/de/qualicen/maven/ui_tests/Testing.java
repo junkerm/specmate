40dbdd51d7a0a152aeddd6917f24664eb948e47d
tobi321 <43335561+tobi321@users.noreply.github.com>
2019-02-20 16:11:53 +0100

    Update Testing.java

--- ui-tests/src/main/java/de/qualicen/maven/ui_tests/Testing.java
+++ ui-tests/src/main/java/de/qualicen/maven/ui_tests/Testing.java
@@ -27,8 +27,8 @@ public class Testing {
 	 
 	    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
 	    
-		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS); //page synchronization
-		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS); //page synchronization
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 	    
 	    // Call two test classes 
 	    TestLoginPage.executeTest(driver);
