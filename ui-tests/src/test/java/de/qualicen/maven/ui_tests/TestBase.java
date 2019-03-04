c5ce4941dc2ec506ef16f38f7c4c2524e4e46e73
Tobias Springer <tobias.springer@tum.de>
2019-03-04 14:48:02 +0100

    Change: Browsers

--- ui-tests/src/test/java/de/qualicen/maven/ui_tests/TestBase.java
+++ ui-tests/src/test/java/de/qualicen/maven/ui_tests/TestBase.java
@@ -62,14 +62,14 @@ public class TestBase implements SauceOnDemandSessionIdProvider {
 
     // Browser configurations
     @ConcurrentParameterized.Parameters
-    public static LinkedList browsersStrings() {
-        LinkedList browsers = new LinkedList();
+    public static LinkedList<String[]> browsersStrings() {
+        LinkedList<String[]> browsers = new LinkedList<String[]>();
 
         browsers.add(new String[]{"Windows 10", "14.14393", "MicrosoftEdge", null, null});
         browsers.add(new String[]{"Windows 10", "49.0", "firefox", null, null});
         browsers.add(new String[]{"Windows 7", "11.0", "internet explorer", null, null});
-        browsers.add(new String[]{"OS X 10.11", "10.0", "safari", null, null});
-        browsers.add(new String[]{"OS X 10.10", "54.0", "chrome", null, null});
+        browsers.add(new String[]{"Windows 10", "11.0", "internet explorer", null, null});
+        browsers.add(new String[]{"Windows 10", "54.0", "chrome", null, null});
         return browsers;
     }
 
