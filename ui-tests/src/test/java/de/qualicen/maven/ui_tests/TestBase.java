f8c0998090c6f99b03fbd03f74605f217a3ac6b2
Tobias Springer <tobias.springer@tum.de>
2019-03-04 16:48:17 +0100

    Remove Chrome instance due to timeouts

--- ui-tests/src/test/java/de/qualicen/maven/ui_tests/TestBase.java
+++ ui-tests/src/test/java/de/qualicen/maven/ui_tests/TestBase.java
@@ -69,7 +69,6 @@ public class TestBase implements SauceOnDemandSessionIdProvider {
         browsers.add(new String[]{"Windows 10", "14.14393", "MicrosoftEdge", null, null});
         browsers.add(new String[]{"Windows 7", "11.0", "internet explorer", null, null});
         browsers.add(new String[]{"Windows 10", "11.0", "internet explorer", null, null});
-        browsers.add(new String[]{"Windows 10", "54.0", "chrome", null, null});
         return browsers;
     }
 
