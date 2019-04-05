f57c5e833a8f3cddf5bbbbc4cb8cdb7cdeeb8346
Sebastian Eder <23170307+sebeder@users.noreply.github.com>
2019-04-05 13:07:59 +0200

    Enabling save button sometimes

--- web/src/app/validation/validation-result.ts
+++ web/src/app/validation/validation-result.ts
@@ -1,14 +1,17 @@
 import { IContainer } from '../model/IContainer';
 import { ValidationMessage } from './validation-message';
+import { ValidationErrorSeverity } from './validation-error-severity';
 
 export class ValidationResult {
     public message: ValidationMessage;
-    constructor(message: string|ValidationMessage, public isValid: boolean, public elements: IContainer[]) {
+    constructor(message: string|ValidationMessage,
+        public isValid: boolean, public elements: IContainer[],
+            public severity: ValidationErrorSeverity = ValidationErrorSeverity.SAVE_ENABLED) {
         if (typeof message === 'string') {
             this.message = new ValidationMessage(message);
         } else {
             this.message = message;
         }
     }
-    public static VALID: ValidationResult = new ValidationResult('', true, []);
+    public static VALID: ValidationResult = new ValidationResult('', true, [], ValidationErrorSeverity.IGNORE);
 }
