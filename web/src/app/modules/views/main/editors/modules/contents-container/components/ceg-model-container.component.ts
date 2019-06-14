274ecf1f30b9cad28bdbd100d180cd6caf694415
Dominik <public@dominik-spies.de>
2019-06-14 14:25:18 +0200

    Added Requirement Textarea to overview

--- web/src/app/modules/views/main/editors/modules/contents-container/components/ceg-model-container.component.ts
+++ web/src/app/modules/views/main/editors/modules/contents-container/components/ceg-model-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { CEGModelFactory } from '../../../../../../../factory/ceg-model-factory';
 import { ModelFactoryBase } from '../../../../../../../factory/model-factory-base';
@@ -19,7 +19,6 @@ import { TestSpecificationContentContainerBase } from '../base/testspecification
     templateUrl: 'ceg-model-container.component.html',
     styleUrls: ['ceg-model-container.component.css']
 })
-
 export class CEGModelContainer extends TestSpecificationContentContainerBase<CEGModel> {
 
     constructor(dataService: SpecmateDataService,
@@ -31,11 +30,30 @@ export class CEGModelContainer extends TestSpecificationContentContainerBase<CEG
         super(dataService, navigator, translate, modal, clipboardService, additionalInformationService);
     }
 
+    modelDescription: string;
+
+
     protected condition = (element: IContainer) => Type.is(element, CEGModel);
 
     public async createElement(name: string): Promise<CEGModel> {
         let factory: ModelFactoryBase = new CEGModelFactory(this.dataService);
-        const element = await factory.create(this.parent, true, Id.uuid, name);
-        return element as CEGModel;
+        const element = await factory.create(this.parent, true, Id.uuid, name) as CEGModel;
+        if (this.modelDescription != undefined && this.modelDescription.trim().length > 0) {
+            element.modelRequirements = this.modelDescription.trim();
+            await this.dataService.updateElement(element, true, Id.uuid);
+            await this.dataService.commit(this.translate.instant('save'));
+            await this.dataService.performOperations(element.url, 'generateModel');
+            await this.dataService.deleteCachedContent(element.url);
+            await this.dataService.readElement(element.url, false);
+            const content = await this.dataService.readContents(element.url, false);
+            if (content.length == 0) {
+                // Nothing was generated --> Delete the empty model
+                await this.dataService.deleteElement(element.url, true, Id.uuid);
+                await this.dataService.commit(this.translate.instant('save'));
+                await this.dataService.deleteCachedContent(element.url);
+                return undefined; // Promise.reject('Model could not be generated.');
+            }
+        }
+        return element;
     }
 }
