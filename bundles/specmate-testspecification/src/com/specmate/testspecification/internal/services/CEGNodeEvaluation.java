7021cc658a7c2e85e55594407aeca6279c9c6f9b
Maximilian Junker <maximilian.junker@qualicen.de>
2019-08-06 14:57:13 +0200

    fix: test specification view

--- bundles/specmate-testspecification/src/com/specmate/testspecification/internal/services/CEGNodeEvaluation.java
+++ bundles/specmate-testspecification/src/com/specmate/testspecification/internal/services/CEGNodeEvaluation.java
@@ -3,28 +3,26 @@ package com.specmate.testspecification.internal.services;
 import java.util.Comparator;
 import java.util.TreeMap;
 
-import com.specmate.model.base.IContainer;
 import com.specmate.model.requirements.CEGNode;
 
 @SuppressWarnings("serial")
-class CEGNodeEvaluation extends TreeMap<IContainer, TaggedBoolean> {
-	
-	private static Comparator<IContainer> evaluationComperator = new Comparator<IContainer>() {
-		// Sort each evaluation, so that the order of nodes in an evaluation are lexicographically sorted
+class CEGNodeEvaluation extends TreeMap<CEGNode, TaggedBoolean> {
+
+	private static Comparator<CEGNode> evaluationComperator = new Comparator<CEGNode>() {
+		// Sort each evaluation, so that the order of nodes in an evaluation are
+		// lexicographically sorted
 		@Override
-		public int compare(IContainer c1, IContainer c2) {
-			CEGNode node1 = (CEGNode) c1;
-			CEGNode node2 = (CEGNode) c2;
-			
-			int result = node1.getVariable().compareTo(node2.getVariable());
-			if(result != 0) {
+		public int compare(CEGNode c1, CEGNode c2) {
+
+			int result = c1.getVariable().compareTo(c2.getVariable());
+			if (result != 0) {
 				return result;
 			} else {
-				return node1.getCondition().compareTo(node2.getCondition());
+				return c1.getCondition().compareTo(c2.getCondition());
 			}
 		}
 	};
-	
+
 	public CEGNodeEvaluation() {
 		super(evaluationComperator);
 	}
