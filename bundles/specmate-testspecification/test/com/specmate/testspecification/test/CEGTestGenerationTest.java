8a46da2e1a801a450d59a127d33f9f17194db548
Maximilian Junker <maximilian.junker@qualicen.de>
2019-08-11 19:50:44 +0200

    fix bug where tests are not correctly generated when two effects share the same vairable with different conditoins

--- bundles/specmate-testspecification/test/com/specmate/testspecification/test/CEGTestGenerationTest.java
+++ bundles/specmate-testspecification/test/com/specmate/testspecification/test/CEGTestGenerationTest.java
@@ -21,8 +21,8 @@ import com.specmate.testspecification.internal.services.CEGTestCaseGenerator;
 public class CEGTestGenerationTest {
 
 	@Test
-	public void testCEGTestGeneration() throws SpecmateException {
-		TestSpecification spec = getTestSpecification();
+	public void testCEGTestGenerationRemoveNot() throws SpecmateException {
+		TestSpecification spec = getTestSpecificationRemoveNot();
 		CEGTestCaseGenerator generator = new CEGTestCaseGenerator(spec);
 		generator.generate();
 		List<TestCase> testcases = SpecmateEcoreUtil.pickInstancesOf(spec.getContents(), TestCase.class);
@@ -33,7 +33,7 @@ public class CEGTestGenerationTest {
 		}));
 	}
 
-	private TestSpecification getTestSpecification() {
+	private TestSpecification getTestSpecificationRemoveNot() {
 		RequirementsFactory f = RequirementsFactory.eINSTANCE;
 		CEGModel ceg = f.createCEGModel();
 		ceg.setId("ceg");
@@ -70,6 +70,83 @@ public class CEGTestGenerationTest {
 		spec.setId("testspec");
 
 		ceg.getContents().addAll(Arrays.asList(trueNode, falseNode, trueEffect, falseEffect, conn1, conn2, spec));
+		return spec;
+	}
+
+	@Test
+	public void testCEGTestGenerationSameVariable() throws SpecmateException {
+		TestSpecification spec = getTestSpecificationSameVariable();
+		CEGTestCaseGenerator generator = new CEGTestCaseGenerator(spec);
+		generator.generate();
+		List<TestCase> testcases = SpecmateEcoreUtil.pickInstancesOf(spec.getContents(), TestCase.class);
+		Assert.assertTrue(testcases.stream().anyMatch(tc -> {
+			List<ParameterAssignment> assignments = SpecmateEcoreUtil.pickInstancesOf(tc.getContents(),
+					ParameterAssignment.class);
+			return assignments.stream().anyMatch(a -> a.getCondition().startsWith("=A"));
+		}));
+		Assert.assertTrue(testcases.stream().anyMatch(tc -> {
+			List<ParameterAssignment> assignments = SpecmateEcoreUtil.pickInstancesOf(tc.getContents(),
+					ParameterAssignment.class);
+			return assignments.stream().anyMatch(a -> a.getCondition().startsWith("=B"));
+		}));
+		Assert.assertTrue(testcases.stream().anyMatch(tc -> {
+			List<ParameterAssignment> assignments = SpecmateEcoreUtil.pickInstancesOf(tc.getContents(),
+					ParameterAssignment.class);
+			return assignments.stream().anyMatch(a -> a.getCondition().startsWith("not =A,not =B"));
+		}));
+	}
+
+	private TestSpecification getTestSpecificationSameVariable() {
+		RequirementsFactory f = RequirementsFactory.eINSTANCE;
+		CEGModel ceg = f.createCEGModel();
+		ceg.setId("ceg");
+
+		CEGNode aNode = f.createCEGNode();
+		aNode.setId("aNode");
+		aNode.setVariable("a");
+		aNode.setCondition("present");
+
+		CEGNode bNode = f.createCEGNode();
+		bNode.setId("bNode");
+		bNode.setVariable("b");
+		bNode.setCondition("present");
+
+		CEGNode cNode = f.createCEGNode();
+		cNode.setId("cNode");
+		cNode.setVariable("c");
+		cNode.setCondition("present");
+
+		CEGNode aEffect = f.createCEGNode();
+		aEffect.setId("aEffect");
+		aEffect.setVariable("v");
+		aEffect.setCondition("=A");
+
+		CEGNode bEffect = f.createCEGNode();
+		bEffect.setId("bEffect");
+		bEffect.setVariable("v");
+		bEffect.setCondition("=B");
+
+		CEGConnection conn1 = f.createCEGConnection();
+		conn1.setSource(aNode);
+		conn1.setTarget(aEffect);
+
+		CEGConnection conn2 = f.createCEGConnection();
+		conn2.setSource(bNode);
+		conn2.setTarget(bEffect);
+
+		CEGConnection conn3 = f.createCEGConnection();
+		conn3.setSource(cNode);
+		conn3.setTarget(aEffect);
+
+		CEGConnection conn4 = f.createCEGConnection();
+		conn4.setSource(cNode);
+		conn4.setTarget(bEffect);
+
+		TestSpecification spec = TestspecificationFactory.eINSTANCE.createTestSpecification();
+		spec.setId("testspec");
+
+		ceg.getContents()
+				.addAll(Arrays.asList(aNode, bNode, cNode, aEffect, bEffect, conn1, conn2, conn3, conn4, spec));
 		return spec;
 	}
 }
