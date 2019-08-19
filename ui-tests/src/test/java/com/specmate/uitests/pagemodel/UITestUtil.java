ac2a2b9627b5a9247ef6263ac6f42be82067a6e0
Tobias Springer <tobias.springer@tum.de>
2019-08-19 13:16:32 +0200

    Change modal method

--- ui-tests/src/test/java/com/specmate/uitests/pagemodel/UITestUtil.java
+++ ui-tests/src/test/java/com/specmate/uitests/pagemodel/UITestUtil.java
@@ -1,6 +1,7 @@
 package com.specmate.uitests.pagemodel;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,11 +11,14 @@ public class UITestUtil {
 	public static void waitForModalToDisappear(WebDriver driver) {
 		By modalLocator = By.id("loading-modal");
 		try {
-			new WebDriverWait(driver, 1).until(ExpectedConditions.visibilityOfElementLocated(modalLocator));
+			driver.findElement(modalLocator).isDisplayed();
 		} catch (TimeoutException te) {
 			// Modal did not appear
 			return;
+		} catch (NoSuchElementException ne) {
+			// Modal did not appear
+			return; 
 		}
-		new WebDriverWait(driver, 5).until(ExpectedConditions.invisibilityOfElementLocated(modalLocator));
+		new WebDriverWait(driver, 10).until(ExpectedConditions.invisibilityOfElementLocated(modalLocator));
 	}
 }
