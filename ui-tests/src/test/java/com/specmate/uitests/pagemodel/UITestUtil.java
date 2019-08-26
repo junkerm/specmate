d55caf20c2f60b86b1e7ac4863889406e154a185
Tobias Springer <tobias.springer@tum.de>
2019-08-26 13:01:06 +0200

    Remove: unused import

--- ui-tests/src/test/java/com/specmate/uitests/pagemodel/UITestUtil.java
+++ ui-tests/src/test/java/com/specmate/uitests/pagemodel/UITestUtil.java
@@ -1,18 +1,14 @@
 package com.specmate.uitests.pagemodel;
 
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UITestUtil {
