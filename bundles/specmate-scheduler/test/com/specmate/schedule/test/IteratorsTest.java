2302fbd4db2dea9fa97eaf73cea9be946bd85904
Tobias Springer <tobias.springer@tum.de>
2019-08-29 09:30:03 +0200

    Workcommit

--- bundles/specmate-scheduler/test/com/specmate/schedule/test/IteratorsTest.java
+++ bundles/specmate-scheduler/test/com/specmate/schedule/test/IteratorsTest.java
@@ -14,6 +14,69 @@ public class IteratorsTest {
 	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	@Test
+	public void testYearlyIterator() throws Exception {
+		Date date = dateFormat.parse("2018-11-01 09:30:10");
+
+		// scheduled time before current time
+		ScheduleIterator hourlyIterator = SchedulerIteratorFactory.create("hour 20 15", date);
+		Date next = hourlyIterator.next();
+		Assert.assertEquals("2018-11-01 10:20:15", dateFormat.format(next));
+
+		// scheduled time after current time
+		hourlyIterator = SchedulerIteratorFactory.create("hour 40 15", date);
+		next = hourlyIterator.next();
+		Assert.assertEquals("2018-11-01 09:40:15", dateFormat.format(next));
+
+		Date endOfDay = dateFormat.parse("2018-12-31 23:30:10");
+		// scheduled time need day jump
+		hourlyIterator = SchedulerIteratorFactory.create("hour 20 15", endOfDay);
+		next = hourlyIterator.next();
+		Assert.assertEquals("2019-01-01 00:20:15", dateFormat.format(next));
+	}
+	
+	@Test
+	public void testMonthlyIterator() throws Exception {
+		Date date = dateFormat.parse("2018-11-01 09:30:10");
+
+		// scheduled time before current time
+		ScheduleIterator hourlyIterator = SchedulerIteratorFactory.create("month 4 40 50", date);
+		Date next = hourlyIterator.next();
+		Assert.assertEquals("2018-12-01 4:40:50", dateFormat.format(next));
+
+		// scheduled time after current time
+		hourlyIterator = SchedulerIteratorFactory.create("month 4 40 50", date);
+		next = hourlyIterator.next();
+		Assert.assertEquals("2018-11-01 04:40:50", dateFormat.format(next));
+
+		Date endOfDay = dateFormat.parse("2018-12-31 23:30:10");
+		// scheduled time need day jump
+		hourlyIterator = SchedulerIteratorFactory.create("month 4 40 50", endOfDay);
+		next = hourlyIterator.next();
+		Assert.assertEquals("2019-01-01 04:40:50", dateFormat.format(next));
+	}
+	
+	@Test
+	public void testWeeklyIterator() throws Exception {
+		Date date = dateFormat.parse("2018-11-01 09:30:10");
+
+		// scheduled time before current time
+		ScheduleIterator hourlyIterator = SchedulerIteratorFactory.create("week 7 45 59", date);
+		Date next = hourlyIterator.next();
+		Assert.assertEquals("2018-11-08 07:45:59", dateFormat.format(next));
+
+		// scheduled time after current time
+		hourlyIterator = SchedulerIteratorFactory.create("week 11 45 59", date);
+		next = hourlyIterator.next();
+		Assert.assertEquals("2018-11-08 11:45:59", dateFormat.format(next));
+
+		Date endOfDay = dateFormat.parse("2018-12-31 23:30:10");
+		// scheduled time need day jump
+		hourlyIterator = SchedulerIteratorFactory.create("week 2 45 59", endOfDay);
+		next = hourlyIterator.next();
+		Assert.assertEquals("2019-01-07 02:45:59", dateFormat.format(next));
+	}
+	
+	@Test
 	public void testHourlyIterator() throws Exception {
 		Date date = dateFormat.parse("2018-11-01 09:30:10");
 
